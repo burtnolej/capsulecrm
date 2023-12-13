89c14e1cbb70cc0fa6a293244af2d6b7de3e431c
Jon Butler <burtnolej@goglemail.com>
2023-12-13 12:17:09 +0000

    init

--- .git-push.sh
+++ .git-push.sh
@@ -22,15 +22,22 @@ fi
 
 tmp="$(git ls-remote | grep $2 2>&1)" 	
 num_valid_repo="$(echo $tmp | wc -l)"
-num_changes=`git status --short | grep " M " | wc -l`
+num_changes=`git status --short | grep " A " | wc -l`
 
-echo "is $2 a valid repo "$num_valid_repo
+if [ $num_valid_repo -ne 1 ]; then
+        echo "not valid repo"
+else
+	echo "yes $2 is a valid repo "
+fi
+
 echo "number of changes are "$num_changes
 
 if [ $3 != "test" ]; then
-	git -m commit $1
+	git status --short | grep " A "
+	git commit -m $1
 	eval "$(ssh-agent -s)"
-	ssh-add ~/.ssh/id_ed25519
+	ssh-add ~/.ssh/id_ed25519_j
+	b
 	git push $2
 else
 	echo "in test mode"
