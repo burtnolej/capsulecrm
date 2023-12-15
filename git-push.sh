3b58304a026bba0670ab0a0921f05ece2e5c6c0b
Jon Butler <burtnolej@goglemail.com>
2023-12-15 18:02:03 +0000

    git push init

--- git-push.sh
+++ git-push.sh
@@ -22,7 +22,8 @@ fi
 
 tmp="$(git ls-remote | grep $2 2>&1)" 	
 num_valid_repo="$(echo $tmp | wc -l)"
-num_changes=`git status --short | grep " A " | wc -l`
+num_changes=`git status --short | grep " M " | wc -l`
+num_adds=`git status --short | grep " A " | wc -l`
 
 if [ $num_valid_repo -ne 1 ]; then
         echo "not valid repo"
@@ -31,10 +32,15 @@ else
 fi
 
 echo "number of changes are "$num_changes
+echo "number of adds are "$num_adds
+
+if [ "${num_changes}" -eq 0 ] && [ "${num_adds}" -eq 0 ]; then
+	echo "no adds or changes to commit"
+	exit
+fi
 
 if [ $3 != "test" ]; then
 	git diff HEAD  --name-only
-	
 	eval "$(ssh-agent -s)"
 	ssh-add ~/.ssh/id_ed25519_jb
 	git push $2
